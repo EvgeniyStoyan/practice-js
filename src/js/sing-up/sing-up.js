5315d4c50748d4c21530380484c10959f79f672e
Evgeniy Stoyan <68951809+EvgeniyStoyan@users.noreply.github.com>
2023-05-11 12:59:37 +0300

    edit

--- src/js/sing-up/sing-up.js
+++ src/js/sing-up/sing-up.js
@@ -1,5 +1,6 @@
-const modalOpen = document.querySelector(".auth__modal-open");
-const modalClose = document.querySelector(".modal-close");
+const signUpBtnMobile = document.querySelector(".sign-up-btn-js")
+const modalOpenBtn = document.querySelector(".auth__modal-open");
+const modalCloseBtn = document.querySelector(".modal-close");
 const modal = document.querySelector(".modal");
 
 const registrationForm = document.querySelector('.registration_form');
@@ -8,8 +9,21 @@ const registrationLink = document.querySelector('.registration_link');
 const loginForm = document.querySelector('.login_form'); 
 const loginLink = document.querySelector('.login_link');
 
-modalOpen.addEventListener('click', toggleModal);
-modalClose.addEventListener('click', toggleModal);
+const btnChangeTheme= document.querySelector('.switch');
+const modalSignUp= document.querySelector('.container_forms');
+const signUpInput= document.querySelector('.sign_up_input');
+const signUpBtn= document.querySelector('.sing_up_btn');
+const iconEmail= document.querySelector('.icon-email');
+const iconLock= document.querySelector('.icon-lock');
+const iconCross= document.querySelector('.icon-cross');
+const modalCross= document.querySelector('.modal_cross');
+
+
+
+signUpBtnMobile.addEventListener('click', toggleModal);
+modalOpenBtn.addEventListener('click', toggleModal);
+modalCloseBtn.addEventListener('click', toggleModal);
+
 
 function toggleModal() {
     document.body.classList.toggle("modal-open");
@@ -33,3 +47,45 @@ function toLogin() {
 }
 
 loginLink.addEventListener("click", toLogin)
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  modalSignUp.classList.add(Theme.DARK);
+  signUpInput.classList.add(Theme.DARK);
+  signUpBtn.classList.add(Theme.DARK);
+  iconEmail.classList.add(Theme.DARK);
+  iconLock.classList.add(Theme.DARK);
+  iconCross.classList.add(Theme.DARK);
+  modalCross.classList.add(Theme.DARK);
+  localStorage.setItem('theme', Theme.DARK);
+  btnChangeTheme.checked = true;
+}
+
+function onInputChangeTheme(e) {
+  if (btnChangeTheme.checked) {
+    modalSignUp.classList.add(Theme.DARK);
+    signUpInput.classList.add(Theme.DARK);
+    signUpBtn.classList.add(Theme.DARK);
+    iconEmail.classList.add(Theme.DARK);
+    iconLock.classList.add(Theme.DARK);
+    iconCross.classList.add(Theme.DARK);
+    modalCross.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    modalSignUp.classList.remove(Theme.DARK);
+    signUpInput.classList.remove(Theme.DARK);
+    signUpBtn.classList.remove(Theme.DARK);
+    iconEmail.classList.remove(Theme.DARK);
+    iconLock.classList.remove(Theme.DARK);
+    iconCross.classList.remove(Theme.DARK);
+    modalCross.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+btnChangeTheme.addEventListener('change', onInputChangeTheme);
